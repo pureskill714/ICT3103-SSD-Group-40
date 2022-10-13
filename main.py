23b9a8af07a339ba379cc1ca3a69072f5ca497ec
Aloysius Chong <aloysiuscjf@gmail.com>
2022-10-14 00:44:41 +0800

    Login and Register functions implemented with stored procedures

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 # Allow users to pass variables into our view function and then dynamically change what we have on our view page
 # Dynamically pass variables into the URL
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 from flask_sqlalchemy import SQLAlchemy  # to create db and an instance of sql Alchemy
 from flask_login import UserMixin, LoginManager, login_required, login_user, logout_user, current_user
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, IntegerField, EmailField, validators
 from wtforms.validators import InputRequired, Length, ValidationError, Email
 from flask_bcrypt import Bcrypt
+import pymssql
 
 app = Flask(__name__, static_url_path='/static')  # Create an instance of the flask app and put in variable app
 app.config['SECRET_KEY'] = 'thisisasecretkey'  # flask uses secret to secure session cookies and protect our webform
 # against attacks such as Cross site request forgery (CSRF)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 
 # Handling the login validation for Customers
@@ -21,76 +20,46 @@ login_manager.init_app(app)
 login_manager.login_view = "login"
 login_manager.login_message = u"Username or Password incorrect. Please try again"
 
-
-# This user loaded callback is used to reload the user object from the user id stored in the session
+#not sure what this does, can we remove?
 @login_manager.user_loader
 def load_user_customer(user_id):
-    return User.query.get(user_id)
-
-
-# a class as a model. The model will represent the data that will be stored in the database
-# this class needs to inherit from db.Model
-class User(db.Model, UserMixin):  # UserMixin is for validating users
-    id = db.Column(db.Integer, primary_key=True)  # This will be primary key
-    firstname = db.Column(db.String(30), nullable=False)
-    lastname = db.Column(db.String(30), nullable=False)
-    email = db.Column(db.String(30), nullable=False, unique=True)
-    username = db.Column(db.String(30), nullable=False, unique=True)  # username field is unique
-    password = db.Column(db.String(80), nullable=False)  # password field
-    contact = db.Column(db.Integer(), nullable=False)
-    role = db.Column(db.String(30), nullable=False)
-    emp_no = db.Column(db.Integer(), nullable=True)
-
+    return 1
 
 class RegisterForm(FlaskForm):
     # For users to choose a first name
     firstname = StringField(validators=[InputRequired(),
-                                        Length(min=4, max=20)])
+                                        Length(min=2, max=64)])
     # For users to choose a last name
     lastname = StringField(validators=[InputRequired(),
-                                       Length(min=4, max=20)])
+                                       Length(min=2, max=64)])
 
     # For users to input their email
     email = EmailField(validators=[InputRequired("Please enter email address"),
-                                   Length(min=4, max=40), Email()])
+                                   Length(min=4, max=254), Email()])
 
     # For users to choose a username
     username = StringField(validators=[InputRequired(),
-                                       Length(min=4, max=20)])
+                                       Length(min=4, max=32)])
     # For users to choose a password
     password = PasswordField(label='Password', validators=[InputRequired(),
-                                                           validators.Length(min=6, max=10),
+                                                           validators.Length(min=8, max=64),
                                                            validators.EqualTo('password_confirm',
                                                                               message='Passwords must match,Please try again')])
 
     # For users to confirm password
     password_confirm = PasswordField(label='Password confirm', validators=[InputRequired(),
-                                                                           validators.Length(min=6, max=10)])
-
-    # For users to enter their contact number
-    contact = IntegerField(validators=[InputRequired()])
+                                                                           validators.Length(min=8, max=32)])
 
     submit = SubmitField("Register")  # Register button once they are done
 
-    # This method checks if there already has an existing username in the database
-    def validate_username(self, username):
-        # This variable query the database table by the username and check if got same username or not
-        existing_user_username = User.query.filter_by(username=username.data).first()
-        if existing_user_username:
-            raise ValidationError("This username already exists. Please choose a different one.")
-
-    def validate_email(self, email):
-        existing_email = User.query.filter_by(email=email.data).first()
-        if existing_email:
-            raise ValidationError("This email already exists in the system. Please register with another")
 
 
 class LoginForm(FlaskForm):
     username = StringField(validators=[InputRequired(),
-                                       Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+                                       Length(min=4, max=32)], render_kw={"placeholder": "Username"})
 
     password = PasswordField(validators=[InputRequired(),
-                                         Length(min=4, max=20)], render_kw={"placeholder": "Password"})
+                                         Length(min=8, max=64)], render_kw={"placeholder": "Password"})
 
     submit = SubmitField("Login")
 
@@ -106,43 +75,44 @@ def login():
     form = LoginForm()
     # check if the user exists in the database
     if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        # If they are in the database,check for their password hashed,compare with real password. If it matches,
-        # then redirect them to dashboard page
-        if user:
-            if user.role == "Customer":
-                if bcrypt.check_password_hash(user.password, form.password.data):
-                    login_user(user)
-                    return redirect(url_for('customerdashboard'))
-
-            if user.role == "Staff":
-                # if bcrypt.check_password_hash(user.password, form.password.data):
-                login_user(user)
-                return redirect(url_for('staffdashboard'))
-
-            if user.role == "Manager":
-                # if bcrypt.check_password_hash(user.password, form.password.data):
-                login_user(user)
-                return redirect(url_for('managerdashboard'))
+        hashed_password = bcrypt.generate_password_hash(form.password.data)
+        
+        #Creating connections individually to avoid open connections
+        #CHANGE TO YOUR OWN MSSQL SERVER PLEASE
+        conn = pymssql.connect("DESKTOP-7GS9BE8", 'sa', '12345678', "3203")
+        cursor = conn.cursor()
 
 
+        #Fetches original password hash of the user
+        #Might replace this execute command with a function for higher security
+        cursor.execute('SELECT Password FROM Users WHERE Username = %s', form.username.data) 
+
+        #Uses bcrypt to check the password hashes and calls the login_user stored procedure to 
+        res = cursor.callproc('login_user', (form.username.data, (int(bcrypt.check_password_hash(cursor.fetchone()[0], form.password.data))), request.remote_addr, pymssql.output(str), pymssql.output(int)))
+        
+        conn.commit()
+        conn.close()
+
+        user_email = res[3] #used for MFA function
+        role_ID = res[4] #used for Flask-Authorize to properly authorize users
+
+        if user_email is not None: #Login was successful
+            #Add code her with Flask-Authorize to determine the role of the user and redirect accordingly
+            return redirect(url_for('customerdashboard'))
         else:
             flash("Username or Password incorrect. Please try again")
 
     return render_template('login.html', form=form)
-
 
 @app.route("/customerdashboard", methods=['GET', 'POST'])
 @login_required  # ensure is logged then, only then can access the dashboard
 def customerdashboard():
     return render_template('dashboards/customerdashboard.html')
 
-
 @app.route("/staffdashboard", methods=['GET', 'POST'])
 @login_required  # ensure is logged then, only then can access the dashboard
 def staffdashboard():
     return render_template('dashboards/staffdashboard.html')
-
 
 @app.route("/managerdashboard", methods=['GET', 'POST'])
 @login_required  # ensure is logged then, only then can access the dashboard
@@ -169,27 +139,28 @@ def register():
     # Whenever we submit this form, we immediately create a hash version of the password and submit to database
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data)
-        new_customer = User(firstname=form.firstname.data, lastname=form.lastname.data,
-                            email=form.email.data, username=form.username.data,
-                            password=hashed_password, contact=form.contact.data,
-                            role="Customer")
-        db.session.add(new_customer)
-        db.session.commit()
-        return redirect(url_for('registersuccess'))  # redirect to login page after register
 
-    # if account creation is successful, go to login page, else flash message to user
-    if request.method == 'POST':
-        if form.validate():
-            return redirect(url_for('registersuccess'))
+        #Creating connections individually to avoid open connections
+        #CHANGE TO YOUR OWN MSSQL SERVER PLEASE
+        conn = pymssql.connect("DESKTOP-7GS9BE8", 'sa', '12345678', "3203")
+
+        cursor = conn.cursor()
+        
+        #Procedure for registration
+        res = cursor.callproc('register_customer', (form.username.data, hashed_password, form.email.data, form.firstname.data, form.lastname.data, pymssql.output(int),))
+        
+        conn.commit()
+        conn.close()
+        
+        if res[5] == 2:
+            # Stored procedure was ran successfully and 
+            return redirect(url_for('registersuccess'))  # redirect to login page after register
+        elif res[5] == 1:
+            # Stored procedure was ran but failed because username or email is already in use
+            flash("Username or Email may already be in use. Please try again. ") 
         else:
-            # Check if username already exists in database and return error message
-            existing_user_username = User.query.filter_by(username=form.username.data).first()
-            if existing_user_username:
-                flash("This username already exists. Please choose a different one.")
-
-            existing_email = User.query.filter_by(email=form.email.data).first()
-            if existing_email:
-                flash("This email already exists in the system. Please register with another.")
+            # Somehow the stored procedure did not run for whatever reason
+            flash("Username or Email may already be in use. Please try again. ")
 
     return render_template('register.html', form=form)
 
@@ -197,15 +168,15 @@ def register():
 @app.route('/customertable')
 @login_required  # ensure is logged then, only then can log out
 def customertable():
-    users = User.query.filter_by(role="Customer")
-    return render_template('tables/customertable.html', users=users)
+    # Implement authorization with Flask-Authorize
+    return render_template('tables/customertable.html')
 
 
 @app.route('/stafftable')
 @login_required  # ensure is logged then, only then can log out
 def stafftable():
-    users = User.query.filter_by(role="Staff")
-    return render_template('tables/stafftable.html', users=users)
+    # Implement authorization with Flask-Authorize
+    return render_template('tables/stafftable.html')
 
 
 @app.route('/registersuccess')
