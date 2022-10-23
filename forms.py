31f9d6bbc3a64e572fa3a644516e31ddf148fcd2
= <=>
2022-10-23 17:59:59 +0800

    booking implemented and forms.py created

---
+++ forms.py
@@ -0,0 +1,62 @@
+from flask_wtf import FlaskForm, RecaptchaField
+from wtforms import StringField, PasswordField, SubmitField, IntegerField, EmailField, validators, SelectField,DateField
+from wtforms.validators import InputRequired, Length, ValidationError, Email, DataRequired
+import datetime
+from datetime import date
+from flask import flash
+
+class RegisterForm(FlaskForm):
+    # For users to choose a first name
+    firstname = StringField(validators=[InputRequired(),
+                                        Length(min=2, max=64)])
+    # For users to choose a last name
+    lastname = StringField(validators=[InputRequired(),
+                                       Length(min=2, max=64)])
+
+    # For users to input their email
+    email = EmailField(validators=[InputRequired("Please enter email address"),
+                                   Length(min=4, max=254), Email()])
+
+    # For users to choose a username
+    username = StringField(validators=[InputRequired(),
+                                       Length(min=4, max=32)])
+    # For users to choose a password
+    password = PasswordField(label='Password', validators=[InputRequired(),
+                                                           validators.Length(min=8, max=64),
+                                                           validators.EqualTo('password_confirm',
+                                                                              message='Passwords must match,Please try again')])
+
+    # For users to confirm password
+    password_confirm = PasswordField(label='Password confirm', validators=[InputRequired(),
+                                                                           validators.Length(min=8, max=32)])
+
+    submit = SubmitField("Register")  # Register button once they are done
+
+
+class LoginForm(FlaskForm):
+    username = StringField(validators=[InputRequired(),
+                                       Length(min=4, max=32)], render_kw={"placeholder": "Username"})
+
+    password = PasswordField(validators=[InputRequired(),
+                                         Length(min=8, max=64)], render_kw={"placeholder": "Password"})
+
+    # For users to enter recaptcha field
+    recaptcha = RecaptchaField()
+
+    submit = SubmitField("Login")
+
+
+class BookingForm(FlaskForm):
+    room_type = SelectField(u'Room Type', choices=[('sdtwin', 'Standard Twin'), ('sdqueen', 'Standard Queen'), ('deluxe', 'Deluxe')])
+    today = date.today()
+    start_date = DateField('Start Date', format='%Y-%m-%d', default=today, validators=(validators.DataRequired(),))
+    end_date = DateField('End date', validators=[DataRequired()])
+    submit = SubmitField('Book')
+
+    def validate_start_date(self, date):
+        if self.start_date.data < datetime.datetime.now().date():
+            raise ValidationError('You can only book for day from today.')
+
+    def validate_end_date(self, date):
+        if self.end_date.data < self.start_date.data:
+            raise ValidationError('You can only select end date after start date.')
--- main.py
+++ main.py
@@ -3,11 +3,9 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy  # to create db and an instance of sql Alchemy
 from flask_login import UserMixin, LoginManager, login_required, login_user, logout_user, current_user
-from flask_wtf import FlaskForm, RecaptchaField
-from wtforms import StringField, PasswordField, SubmitField, IntegerField, EmailField, validators
-from wtforms.validators import InputRequired, Length, ValidationError, Email
 from flask_bcrypt import Bcrypt
 from flask_wtf.csrf import CSRFProtect, CSRFError
+from forms import *
 import pymssql
 
 app = Flask(__name__, static_url_path='/static')  # Create an instance of the flask app and put in variable app
@@ -17,7 +15,7 @@ app.config['RECAPTCHA_PRIVATE_KEY'] = '6LdMHXAiAAAAAP3uAfsgPERmaMdA9ITnVIK1vn9W'
 # against attacks such as Cross site request forgery (CSRF)
 bcrypt = Bcrypt(app)
 
-csrf = CSRFProtect(app) #globally enable csrf protection within the application
+csrf = CSRFProtect(app)  # globally enable csrf protection within the application
 
 # Handling the login validation for Customers
 login_manager = LoginManager()  # Allow our app and flask login to work together
@@ -25,51 +23,11 @@ login_manager.init_app(app)
 login_manager.login_view = "login"
 login_manager.login_message = u"Username or Password incorrect. Please try again"
 
-#not sure what this does, can we remove?
+
+# not sure what this does, can we remove?
 @login_manager.user_loader
 def load_user_customer(user_id):
     return 1
-
-class RegisterForm(FlaskForm):
-    # For users to choose a first name
-    firstname = StringField(validators=[InputRequired(),
-                                        Length(min=2, max=64)])
-    # For users to choose a last name
-    lastname = StringField(validators=[InputRequired(),
-                                       Length(min=2, max=64)])
-
-    # For users to input their email
-    email = EmailField(validators=[InputRequired("Please enter email address"),
-                                   Length(min=4, max=254), Email()])
-
-    # For users to choose a username
-    username = StringField(validators=[InputRequired(),
-                                       Length(min=4, max=32)])
-    # For users to choose a password
-    password = PasswordField(label='Password', validators=[InputRequired(),
-                                                           validators.Length(min=8, max=64),
-                                                           validators.EqualTo('password_confirm',
-                                                                              message='Passwords must match,Please try again')])
-
-    # For users to confirm password
-    password_confirm = PasswordField(label='Password confirm', validators=[InputRequired(),
-                                                                           validators.Length(min=8, max=32)])
-
-    submit = SubmitField("Register")  # Register button once they are done
-
-
-
-class LoginForm(FlaskForm):
-    username = StringField(validators=[InputRequired(),
-                                       Length(min=4, max=32)], render_kw={"placeholder": "Username"})
-
-    password = PasswordField(validators=[InputRequired(),
-                                         Length(min=8, max=64)], render_kw={"placeholder": "Password"})
-
-    #For users to enter recaptcha field
-    recaptcha = RecaptchaField()
-    
-    submit = SubmitField("Login")
 
 
 # App routes help to redirect to different pages of the website
@@ -84,43 +42,47 @@ def login():
     # check if the user exists in the database
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data)
-        
-        #Creating connections individually to avoid open connections
-        #CHANGE TO YOUR OWN MSSQL SERVER PLEASE
-        conn = pymssql.connect("LAPTOP-5NI9K14N", 'sa', '12345678', "3203")
+
+        # Creating connections individually to avoid open connections
+        # CHANGE TO YOUR OWN MSSQL SERVER PLEASE
+        conn = pymssql.connect("DESKTOP-FDNFHQ1", 'sa', 'raheem600', "3103 Hotel")
         cursor = conn.cursor()
 
+        # Fetches original password hash of the user
+        # Might replace this execute command with a function for higher security
+        cursor.execute('SELECT Password FROM Users WHERE Username = %s', form.username.data)
 
-        #Fetches original password hash of the user
-        #Might replace this execute command with a function for higher security
-        cursor.execute('SELECT Password FROM Users WHERE Username = %s', form.username.data) 
+        # Uses bcrypt to check the password hashes and calls the login_user stored procedure to
+        res = cursor.callproc('login_user', (
+            form.username.data, (int(bcrypt.check_password_hash(cursor.fetchone()[0], form.password.data))),
+            request.remote_addr, pymssql.output(str), pymssql.output(int)))
 
-        #Uses bcrypt to check the password hashes and calls the login_user stored procedure to 
-        res = cursor.callproc('login_user', (form.username.data, (int(bcrypt.check_password_hash(cursor.fetchone()[0], form.password.data))), request.remote_addr, pymssql.output(str), pymssql.output(int)))
-        
         conn.commit()
         conn.close()
 
-        user_email = res[3] #used for MFA function
-        role_ID = res[4] #used for Flask-Authorize to properly authorize users
+        user_email = res[3]  # used for MFA function
+        role_ID = res[4]  # used for Flask-Authorize to properly authorize users
 
-        if user_email is not None: #Login was successful
-            #Add code her with Flask-Authorize to determine the role of the user and redirect accordingly
+        if user_email is not None:  # Login was successful
+            # Add code her with Flask-Authorize to determine the role of the user and redirect accordingly
             return redirect(url_for('customerdashboard'))
         else:
             flash("Username or Password incorrect. Please try again")
 
     return render_template('login.html', form=form)
 
+
 @app.route("/customerdashboard", methods=['GET', 'POST'])
 @login_required  # ensure is logged then, only then can access the dashboard
 def customerdashboard():
     return render_template('dashboards/customerdashboard.html')
 
+
 @app.route("/staffdashboard", methods=['GET', 'POST'])
 @login_required  # ensure is logged then, only then can access the dashboard
 def staffdashboard():
     return render_template('dashboards/staffdashboard.html')
+
 
 @app.route("/managerdashboard", methods=['GET', 'POST'])
 @login_required  # ensure is logged then, only then can access the dashboard
@@ -148,24 +110,26 @@ def register():
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data)
 
-        #Creating connections individually to avoid open connections
-        #CHANGE TO YOUR OWN MSSQL SERVER PLEASE
-        conn = pymssql.connect("LAPTOP-5NI9K14N", 'sa', '12345678', "3203")
+        # Creating connections individually to avoid open connections
+        # CHANGE TO YOUR OWN MSSQL SERVER PLEASE
+        conn = pymssql.connect("DESKTOP-FDNFHQ1", 'sa', 'raheem600', "3103 Hotel")
 
         cursor = conn.cursor()
-        
-        #Procedure for registration
-        res = cursor.callproc('register_customer', (form.username.data, hashed_password, form.email.data, form.firstname.data, form.lastname.data, pymssql.output(int),))
-        
+
+        # Procedure for registration
+        res = cursor.callproc('register_customer', (
+            form.username.data, hashed_password, form.email.data, form.firstname.data, form.lastname.data,
+            pymssql.output(int),))
+
         conn.commit()
         conn.close()
-        
+
         if res[5] == 2:
             # Stored procedure was ran successfully and 
             return redirect(url_for('registersuccess'))  # redirect to login page after register
         elif res[5] == 1:
             # Stored procedure was ran but failed because username or email is already in use
-            flash("Username or Email may already be in use. Please try again. ") 
+            flash("Username or Email may already be in use. Please try again. ")
         else:
             # Somehow the stored procedure did not run for whatever reason
             flash("Username or Email may already be in use. Please try again. ")
@@ -192,50 +156,70 @@ def stafftable():
 def registersuccess():
     return render_template('registersucess.html')
 
+
 # 400 - To handle Bad request
 @app.route('/400')
 def error400():
     abort(400)
+
 
 # 401 - To handle error of Unauthorized request
 @app.route('/401')
 def error401():
     abort(401)
 
+
 # 404 - To handle error in matching the Request URL
 @app.route('/404')
 def error404():
     abort(404)
+
 
 # 500 - To handle error in Internal Server Error
 @app.route('/500')
 def error500():
     abort(500)
 
+
 # To direct to 400 page
 @app.errorhandler(400)
 def unauthorized_page(error):
     return render_template('400.html'), 400
-    
+
+
 # To direct to 401 page
 @app.errorhandler(401)
 def unauthorized_page(error):
     return render_template('401.html'), 401
-    
+
+
 # To direct to 404 page
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('404.html'), 404
+
 
 # To direct to 500 page
 @app.errorhandler(500)
 def internal_error(error):
     return render_template('500.html'), 500
 
+
 # To direct to CSRF validation error
 @app.errorhandler(CSRFError)
 def handle_csrf_error(error):
     return render_template('403.html'), 403
+
+
+@app.route("/booking", methods=['GET', 'POST'])
+# @login_required
+def booking():
+    form = BookingForm()
+    if form.validate_on_submit():
+        flash('Booking success!')
+        return redirect(url_for('booking'))
+    return render_template('bookings/bookroom.html', title='Book Rooms', form=form)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
